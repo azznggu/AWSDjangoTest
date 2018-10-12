95edf2800b0e3b113d25882d2832433ce8dcc4b7
azznggu <azznggu@gmail.com>
2018-10-12 09:41:28 +0900

    test edit.

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,8 +1,12 @@
  # helloworld/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from django.http import HttpResponse
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+    def test(self, request):
+        return render(request, 'test.html', context=None)
+    
